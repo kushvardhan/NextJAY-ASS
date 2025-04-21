171479dd91815bd091599c0720acab65a4c590b6
Kush Vardhan <kushvardhan39797@gmail.com>
2025-04-21 21:30:58 +0530

    Initial commit of Next.js project

---
+++ one/src/app/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div className="w-full min-h-[calc(100vh)] flex items-center justify-center">
+      <h1 className="text-2xl">KUSH VARDHAN</h1>
+    </div>
+  );
+}
---
+++ one/src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='w-full h-[10vh] bg-zinc-800 flex justify-center items-center'>
+        <h1 className='text-3xl font-bold text-center text-white'>NextJAY</h1>
+        
+    </div>
+  )
+}
+
+export default Navbar
